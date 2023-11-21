ae985a2796da0260ff97a3035249543bf8cd7a6b
Hariniaina1764! <grace.andrianisoa@gmail.com>
2023-11-21 15:11:30 +0100

    ajout attribut de mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import { donneesTest } from './component/types/Propriete'
 import Tableau from './component/tableau/tableau.component'
 const App = () => {
   return (
