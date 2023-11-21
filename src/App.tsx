3b375034ea88ec2f181cb0391baef868638f67bc
Hariniaina1764! <grace.andrianisoa@gmail.com>
2023-11-21 15:06:26 +0100

    rectification App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Tableau from './component/tableau/tableau.component'
 const App = () => {
   return (
       <div>
-          <h1>Tableau des joueurs</h1>
+          <h1>Tableau Statistiques des joueurs</h1>
           <Tableau  />
       </div>
   );
